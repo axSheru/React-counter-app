a6e5845c4337f9d424bb0a8a1a30f3ff171393ce
Alex Paredes <alexparedesr95@gmail.com>
2022-03-02 21:59:48 -0600

    FEAT: Created examples with Fragments.

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 //Functional Component.
 const PrimeraApp = () => {
-    return <h1>Hola mundo</h1>;
+    return(
+        <>
+            <h1>Hola mundo</h1>
+            <p>Mi primera aplicación.</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
