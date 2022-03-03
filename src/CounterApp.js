f8be49017423c1aeb29d4d122e4a0725c0c8a497
Alex Paredes <alexparedesr95@gmail.com>
2022-03-03 10:10:12 -0600

    FEAT: Added events to subtract and reset to the counter.

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 12 }) => {
 
-    const [ counter, setCounter ] = useState(0);
+    const [ counter, setCounter ] = useState( value );
 
     // HandleAdd
     const handleAdd = () => {
@@ -11,12 +11,20 @@ const CounterApp = ({ value }) => {
         // setCounter( () => c + 1 );// Alternativa.
     };
 
+    // HandleSubtract
+    const handleSubtract = () => setCounter( counter - 1 );
+
+    // HandleReset
+    const handleReset = () => setCounter( value );
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
 
             <button onClick={ handleAdd } >+1</button>
+            <button onClick={ handleReset } >Reset</button>
+            <button onClick={ handleSubtract } >-1</button>
         </>
     );
 
