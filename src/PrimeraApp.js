46724d8dc107f0a51f3bab10692de6333fa12f79
Alex Paredes <alexparedesr95@gmail.com>
2022-03-02 22:28:09 -0600

    FEAT: Added defaultProps section.

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 //Functional Component.
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
     return(
         <>
             <h1>{ saludo }</h1>
-            <p>Mi primera aplicación.</p>
+            <p>{ subtitulo }</p>
         </>
     );
 };
 
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired,
+};
+
+PrimeraApp.defaultProps = {
+    subtitulo: "Soy un subtítulo",
 };
 
 export default PrimeraApp;
